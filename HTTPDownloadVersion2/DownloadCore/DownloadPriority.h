67a2b250e5a0630683094f0adb4fd493165e2e27
LeHuuNghi <nghilh@vng.com.vn>
2018-08-17 10:46:58 +0700

    enum priority

---
+++ HTTPDownloadVersion2/DownloadCore/DownloadPriority.h
@@ -0,0 +1,18 @@
+//
+//  DownloadPriority.h
+//  HTTPDownloadVersion2
+//
+//  Created by CPU11360 on 8/7/18.
+//  Copyright © 2018 CPU11360. All rights reserved.
+//
+
+#ifndef DownloadPriority_h
+#define DownloadPriority_h
+
+typedef NS_ENUM(NSInteger, DownloadPriority){
+    DownloadPriorityLow = 0,
+    DownloadPriorityMedium = 1,
+    DownloadPriorityHigh = 2
+};
+
+#endif /* DownloadPriority_h */
